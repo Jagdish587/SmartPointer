bbef357da6152914947e8a7d90aa802e404f8765
Jagdish587 <t.jag587@gmail.com>
2020-09-20 16:41:43 +0530

    unique pointer changes added

--- uniquPtr1.cpp
+++ uniquPtr1.cpp
@@ -29,21 +29,56 @@ int main()
 
   auto uniqTaskPtr = make_unique<Task>(45);
 
-  auto uniqTasptr2 = move(uniqTaskPtr);
+  auto uniqTasptr2 = move(uniqTaskPtr); // it transfers ownership
 
-  std::cout<<"my id = "<<uniqTasptr2->mId<<endl;
+  if(uniqTaskPtr != nullptr) {
+       std::cout<<"uniqTaskPtr my id = "<<uniqTaskPtr->mId<<endl;
+  } else {
+      std::cout<<"uniqTaskPtr is null  \n";
+  }
 
-  Task *ptr = uniqTasptr2.release(); // destuctor will not be called
+  if(uniqTasptr2 != nullptr) {
+      std::cout<<"uniqTaskPtr initially is not null  \n";
+  } else {
+      std::cout<<"uniqTaskPtr is null  \n";
+  }
 
-  cout<<"value of ptr = "<<ptr->mId<<endl;
+  std::cout<<"uniqTasptr2 my id = "<<uniqTasptr2.get()<<endl;
+  Task *nptr = uniqTasptr2.get(); // returns a pointer
+  std::cout<<"Task *nptr  = "<<nptr->mId<<endl;
 
-  cout<<"destructor not called 1.0 \n ";
+  nptr->mId = 20;
+
+  std::cout<<"uniqTasptr2 my id after change = "<<uniqTasptr2->mId<<endl;
+
+  uniqTasptr2->mId =30;
+
+  std::cout<<"uniqTasptr2 my id 2nd after change = "<<uniqTasptr2->mId<<endl;
+
+  Task *ptr = uniqTasptr2.release(); // releases pointer , destuctor will not be called
+
+  std::cout<<"uniqTasptr2 after release  \n";
+
+  if(uniqTasptr2 != nullptr) {
+      std::cout<<"uniqTasptr2 is not null  \n";
+  } else {
+      std::cout<<"uniqTasptr2 is null  \n";
+  }
+
+  cout<<"value of ptr after release = "<<ptr->mId<<endl;
+
+ // cout<<"destructor not called 1.0 \n ";
 
   auto uniqTaskPtr3 = make_unique<Task>(45);
 
-  uniqTaskPtr3.reset(); // destructor gets called for reset but bot for release
+  uniqTaskPtr3.reset(); // destructor gets called for reset but not for release
+
+  if(uniqTaskPtr3) {
+       std::cout<<"uniqTaskPtr3 is not null  \n";
+  } else {
+       std::cout<<"uniqTaskPtr3 is  null  \n";
+  }
 
 
   return 0;
 }
-
